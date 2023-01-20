2c8fe4875767bd28fb15fab53e78844040823410
caioaugustoo <caioamfr@gmail.com>
2023-01-19 22:25:30 -0300

    fix: typo

--- src/routes/app.go
+++ src/routes/app.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// App will handle defauilt/generics app routes like ``/``, ``/ping``, etc...
+// App will handle default/generics app routes like ``/``, ``/ping``, etc...
 func App(router *fiber.App) {
 	router.Post("/ping", controllers.PingAppController)
 }
