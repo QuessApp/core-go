7e383a10f5c25cf5d2f00f66a3463571d388ce01
caioaugustoo <caioamfr@gmail.com>
2023-02-21 13:39:11 -0300

    chore: load questions route

--- internal/routes/app.go
+++ internal/routes/app.go
@@ -10,10 +10,12 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func LoadRoutes(db *mongo.Database, cfg *configs.Conf, authRepository *repositories.Auth, usersRepository *repositories.Users) {
+func LoadRoutes(db *mongo.Database, cfg *configs.Conf, questionsRepository *repositories.Questions, authRepository *repositories.Auth, usersRepository *repositories.Users) {
 	app := fiber.New()
 	middlewares.LoadMiddlewares(app, cfg)
 
 	LoadAuthRoutes(app, db, authRepository, usersRepository)
+	LoadQuestionsRoute(app, db, questionsRepository, usersRepository)
+
 	log.Fatal(app.Listen(cfg.ServerPort))
 }
