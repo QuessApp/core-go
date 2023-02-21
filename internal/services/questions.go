d495592c69627816a82175d77ba1cdf0b8355a17
caioaugustoo <caioamfr@gmail.com>
2023-02-21 13:55:00 -0300

    feat: validate create question payload

--- internal/services/questions.go
+++ internal/services/questions.go
@@ -9,6 +9,10 @@ import (
 
 // CreateQuestion reads payload from request body then try to create a new question in database.
 func CreateQuestion(payload entities.Question, questionsRepository *repositories.Questions, usersRepository *repositories.Users) (*entities.Question, error) {
+	if err := payload.Validate(); err != nil {
+		return nil, err
+	}
+
 	userToSendQuestion := usersRepository.FindUserByNick(payload.SendTo)
 
 	// TODO: VALIDATE USER IS BLOCKED BY RECEIVER, IS SENDING TO YOURSELF, DID BLOCK RECEIVER, etc.
