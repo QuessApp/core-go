442db19854915b26c6aafe8b32a6c5745f5e5759
caioaugustoo <caioamfr@gmail.com>
2023-02-21 13:41:46 -0300

    feat: create handler to create question

---
+++ internal/handlers/questions.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"core/internal/entities"
+	"core/internal/repositories"
+	"core/internal/services"
+	"core/pkg/responses"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// CreateQuestionHandler is a handler to create a question.
+func CreateQuestionHandler(c *fiber.Ctx, questionsRepository *repositories.Questions, usersRepository *repositories.Users) error {
+	payload := entities.Question{}
+
+	if err := c.BodyParser(&payload); err != nil {
+		return responses.ParseUnsuccesfull(c, http.StatusBadRequest, err.Error())
+	}
+
+	q, err := services.CreateQuestion(payload, questionsRepository, usersRepository)
+
+	if err != nil {
+		return responses.ParseUnsuccesfull(c, http.StatusBadRequest, err.Error())
+	}
+
+	return responses.ParseSuccessful(c, http.StatusCreated, q)
+}
