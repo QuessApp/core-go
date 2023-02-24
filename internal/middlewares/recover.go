bb11937d325a0de7679f6183d7454e510a072599
caioaugustoo <caioamfr@gmail.com>
2023-02-23 23:29:00 -0300

    feat: disable recover middleware in dev env

--- internal/middlewares/recover.go
+++ internal/middlewares/recover.go
@@ -1,12 +1,25 @@
 package middlewares
 
 import (
+	"core/internal/configs"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 // LoadRecoverMiddleware applies recover middleware for all routes.
 // See https://docs.gofiber.io/api/middleware/recover/
-func LoadRecoverMiddleware(app *fiber.App) {
-	app.Use(recover.New())
+func LoadRecoverMiddleware(app *fiber.App, cfg *configs.Conf) {
+	app.Use(recover.New(recover.Config{
+		Next: func(c *fiber.Ctx) bool {
+			shouldSkip := cfg.Env == "development"
+
+			if shouldSkip {
+				log.Println("[DEV] For development purpouses like debugging the recover middleware is disabled.")
+			}
+
+			return shouldSkip
+		},
+	}))
 }
