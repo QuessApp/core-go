9887b6609a551cf6326151b48d3da72dc14ead80
caioaugustoo <caioamfr@gmail.com>
2023-03-08 23:29:23 -0300

    refactor: create variables to rate limiter config

--- internal/middlewares/rate-limit.go
+++ internal/middlewares/rate-limit.go
@@ -9,16 +9,22 @@ import (
 	"github.com/kuriozapp/toolkit/responses"
 )
 
+const (
+	MAX_REQUESTS             = 100
+	TIME_TO_RESET_IN_SECONDS = 30
+	ERROR_MESSAGE            = "Você fez muitas requisições em pouco tempo. Por favor, aguarde alguns segundos."
+)
+
 // ApplyRateLimitMiddleware applies rate limiter middleware for all routes.
 func ApplyRateLimitMiddleware(app *fiber.App) {
 	app.Use(limiter.New(limiter.Config{
-		Max: 100,
+		Max: MAX_REQUESTS,
 		KeyGenerator: func(c *fiber.Ctx) string {
 			return fmt.Sprintf("%s_%s", c.IP(), c.Path())
 		},
-		Expiration: 30 * time.Second,
+		Expiration: TIME_TO_RESET_IN_SECONDS * time.Second,
 		LimitReached: func(c *fiber.Ctx) error {
-			return responses.ParseUnsuccesfull(c, fiber.StatusTooManyRequests, "Você fez muitas requisições em pouco tempo. Por favor, aguarde alguns segundos.")
+			return responses.ParseUnsuccesfull(c, fiber.StatusTooManyRequests, ERROR_MESSAGE)
 		},
 	}))
 }
