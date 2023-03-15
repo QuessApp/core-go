2597396303c5adefbe3c5d77a97affd7ff3a42ed
caioaugustoo <caioamfr@gmail.com>
2023-03-15 14:52:04 -0300

    feat: map user and question in report

--- internal/reports/entities.go
+++ internal/reports/entities.go
@@ -11,7 +11,13 @@ type Report struct {
 	ID        toolkitEntities.ID `json:"id" bson:"_id"`
 	Type      string             `json:"type" bson:"type"`
 	Reason    string             `json:"reason" bson:"reason"`
-	SendTo    toolkitEntities.ID `json:"sendTo" bson:"sendTo"`
+	SendTo    any                `json:"sendTo" bson:"sendTo"`
 	SentBy    toolkitEntities.ID `json:"sentBy" bson:"sentBy"`
 	CreatedAt time.Time          `json:"createdAt" bson:"createdAt,omitempty"`
+}
+
+// PaginatedReports is a model for paginated reports in app.
+type PaginatedReports struct {
+	Reports    *[]Report `json:"reports"`
+	TotalCount int64     `json:"totalCount"`
 }
--- internal/reports/services.go
+++ internal/reports/services.go
@@ -62,13 +62,15 @@ func CreateReport(handlerCtx *configs.HandlersCtx, payload *CreateReportDTO, aut
 }
 
 // FindReportByID retrieves a report with the given ID and verifies whether the authenticated user is authorized to view it.
+// If the report is about a user or a question, this function will also fetch additional data
+// about the reported user/question to be displayed in the UI
 // It takes four parameters: handlerCtx, reportID, authenticatedUserID, and reportsRepository.
 // handlerCtx is an instance of the HandlersCtx struct, which contains the fiber context and application context.
 // reportID is the ID of the report to retrieve.
 // authenticatedUserID is the ID of the authenticated user.
 // reportsRepository is an instance of the ReportsRepository struct, which is used to access and modify report data.
 // It returns a pointer to the Report struct and an error.
-func FindReportByID(handlerCtx *configs.HandlersCtx, reportID, authenticatedUserID toolkitEntities.ID, reportsRepository *ReportsRepository) (*Report, error) {
+func FindReportByID(handlerCtx *configs.HandlersCtx, reportID, authenticatedUserID toolkitEntities.ID, reportsRepository *ReportsRepository, usersRepository *users.UsersRepository, questionsRepository *questions.QuestionsRepository) (*Report, error) {
 	r, err := reportsRepository.FindByID(reportID)
 
 	if err != nil {
@@ -83,7 +85,123 @@ func FindReportByID(handlerCtx *configs.HandlersCtx, reportID, authenticatedUser
 		return nil, err
 	}
 
+	// if the user reported an user
+	// we would like to show who is the reported user
+	// this will help to show in UI
+	if r.Type == "user" {
+		u := usersRepository.FindUserByID(r.SendTo.(toolkitEntities.ID))
+
+		r.SendTo = users.User{
+			ID:        u.ID,
+			Nick:      u.Nick,
+			Name:      u.Name,
+			AvatarURL: u.AvatarURL,
+		}
+	}
+
+	if r.Type == "question" {
+		q := questionsRepository.FindQuestionByID(r.SendTo.(toolkitEntities.ID))
+		u := usersRepository.FindUserByID(q.SentBy.(toolkitEntities.ID))
+
+		// if the user reported a question
+		// we would like to show who sent the reported question
+		// this will help to show in UI
+		r.SendTo = questions.Question{
+			ID:          q.ID,
+			Content:     q.Content,
+			IsAnonymous: q.IsAnonymous,
+			CreatedAt:   q.CreatedAt,
+			SentBy: users.User{
+				ID:        u.ID,
+				Nick:      u.Name,
+				AvatarURL: u.AvatarURL,
+				Name:      u.Name,
+			},
+		}.MapAnonymousFields()
+	}
+
 	return r, nil
+}
+
+// FindAllSent returns a paginated list of reports sent by the authenticated user, sorted by a specified field and order.
+// If the page or sort parameters are nil, they will be set to their default values.
+// The reports are retrieved from the provided ReportsRepository, and the user and question data is retrieved from their respective repositories.
+// If a report refers to a user, its SendTo field will be replaced with the user's data, to be shown in the UI.
+// If a report refers to a question, its SendTo field will be replaced with the question's data, along with the user who sent it, to be shown in the UI.
+// The resulting paginated list of reports is returned, along with an error if one occurs during the retrieval process.
+func FindAllSent(handlerCtx *configs.HandlersCtx, page *int64, sort *string, authenticatedUserID toolkitEntities.ID, reportsRepository *ReportsRepository, usersRepository *users.UsersRepository, questionsRepository *questions.QuestionsRepository) (*PaginatedReports, error) {
+	if *page == 0 {
+		*page = 1
+	}
+
+	if *sort == "" {
+		*sort = "asc"
+	}
+
+	reports, err := reportsRepository.FindAllSentReports(authenticatedUserID, page, sort)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(*reports.Reports) == 0 {
+		return &PaginatedReports{
+			Reports:    &[]Report{},
+			TotalCount: 0,
+		}, nil
+	}
+
+	var allReports []Report
+
+	for _, r := range *reports.Reports {
+		if r.Type == "user" {
+			u := usersRepository.FindUserByID(r.SendTo.(toolkitEntities.ID))
+
+			// if the user reported an user
+			// we would like to show who is the reported user
+			// this will help to show in UI
+			r.SendTo = users.User{
+				ID:        u.ID,
+				Nick:      u.Nick,
+				Name:      u.Name,
+				AvatarURL: u.AvatarURL,
+			}
+
+			allReports = append(allReports, r)
+		}
+
+		if r.Type == "question" {
+			q := questionsRepository.FindQuestionByID(r.SendTo.(toolkitEntities.ID))
+
+			// get sender question data
+			u := usersRepository.FindUserByID(q.SentBy.(toolkitEntities.ID))
+
+			// if the user reported a question
+			// we would like to show who sent the reported question
+			// this will help to show in UI
+			r.SendTo = questions.Question{
+				ID:          q.ID,
+				Content:     q.Content,
+				IsAnonymous: q.IsAnonymous,
+				CreatedAt:   q.CreatedAt,
+				SentBy: users.User{
+					ID:        u.ID,
+					Nick:      u.Name,
+					AvatarURL: u.AvatarURL,
+					Name:      u.Name,
+				},
+			}.MapAnonymousFields()
+
+			allReports = append(allReports, r)
+		}
+	}
+
+	result := PaginatedReports{
+		Reports:    &allReports,
+		TotalCount: reports.TotalCount,
+	}
+
+	return &result, nil
 }
 
 // DeleteReport is responsible for deleting a report.
@@ -118,5 +236,3 @@ func DeleteReport(handlerCtx *configs.HandlersCtx, reportID, authenticatedUserID
 
 	return nil
 }
-
-// TODO: edit report service
