8237df7ee8b4cc99e88a3b6fd66c6b583acafc04
caioaugustoo <caioamfr@gmail.com>
2023-04-01 18:38:20 -0300

    chore: organize folders to handle queues

---
+++ internal/queues/queue.go
@@ -0,0 +1,20 @@
+package queues
+
+import "github.com/streadway/amqp"
+
+// DeclareQueue declares an AMQP queue on the provided channel with the specified queueName.
+// It takes a pointer to an AMQP channel object (ch) and a string (queueName) representing the name of the queue.
+// It uses the ch.QueueDeclare function to declare the queue on the channel, specifying the queueName and other properties such as durability and deletion policy.
+// The function returns a pointer to the declared queue object and an error object if an error occurs.
+func DeclareQueue(ch *amqp.Channel, queueName string) (*amqp.Queue, error) {
+	q, err := ch.QueueDeclare(
+		queueName, // name
+		true,      // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+
+	return &q, err
+}
