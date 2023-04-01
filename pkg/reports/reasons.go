a7ecd9aee16e04275349bac4fa69e4e6adb1bc17
caioaugustoo <caioamfr@gmail.com>
2023-04-01 18:39:44 -0300

    chore: update report reasons variable

---
+++ pkg/reports/reasons.go
@@ -0,0 +1,3 @@
+package reports
+
+var REASONS = "spam, nudity or sexual activity, hate speech or symbols, violence or dangerous organizations, bullying or harassment, selling illegal or regulated goods, intellectual property violations, suicide or self-injury, eating disorders, scams or fraud, false information"
