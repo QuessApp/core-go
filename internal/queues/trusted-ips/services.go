80d636ae487788b776d46cf04712d2e3a22649f9
caioaugustoo <caioamfr@gmail.com>
2023-04-03 15:28:33 -0300

    refactor: change func param name

--- internal/queues/trusted-ips/services.go
+++ internal/queues/trusted-ips/services.go
@@ -15,7 +15,7 @@ import (
 // The IP address is normalized by removing dots (".") and concatenating it with the destination queue name to create the message.
 // The message is then published to the specified AMQP queue using the Publish function from the "queue" package.
 // If there is an error during the publishing process, the function logs a fatal error message with the details of the error.
-func SendIPToQueue(cfg *configs.Conf, ch *amqp.Channel, q *amqp.Queue, ip, sendTo string) {
+func SendIPToQueue(cfg *configs.Conf, ch *amqp.Channel, q *amqp.Queue, ip, sendToEmail string) {
 	isLocalHost := ip == "127.0.0.1" || ip == "0.0.0.0"
 
 	if isLocalHost {
@@ -23,7 +23,7 @@ func SendIPToQueue(cfg *configs.Conf, ch *amqp.Channel, q *amqp.Queue, ip, sendT
 	}
 
 	normalizedIP := strings.ReplaceAll(ip, ".", "")
-	message := fmt.Sprintf("%s-%s", sendTo, normalizedIP)
+	message := fmt.Sprintf("%s-%s", sendToEmail, normalizedIP)
 
 	if err := queue.Publish(ch, q.Name, cfg.Crypto.Key, []byte(message)); err != nil {
 		log.Fatalf("fail to send email to user %s \n", err)
